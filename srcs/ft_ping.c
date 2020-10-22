bbb5161f08f32dc8e4a3fdddd8c06c812d88e470
jdaufin <jdaufin@student.42.fr>
2020-10-22 20:28:24 +0200

    main function syntaxic simplifications

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <jdaufin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 18:29:53 by jdaufin           #+#    #+#             */
-/*   Updated: 2020/10/22 19:51:49 by jdaufin          ###   ########lyon.fr   */
+/*   Updated: 2020/10/22 20:26:02 by jdaufin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ int			main(int argc, char *argv[])
 {
 	char	address[MAX_FQDN];
 
-	ft_bzero(&address[0], MAX_FQDN);
-	if (!parse_args(argc, argv, &address[0]) || g_options.help)
+	ft_bzero(address, MAX_FQDN);
+	if (!parse_args(argc, argv, address) || g_options.help)
 	{
 		show_help();
-		return (EXIT_FAILURE);
+		return (g_options.help ? EXIT_SUCCESS : EXIT_FAILURE);
 	}
-	printf("ft_ping: %s\n", address);
+	printf("PING %s\n", address);
 	return (EXIT_SUCCESS);
 }
