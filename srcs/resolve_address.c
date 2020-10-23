b1ac5a4a80718ed7f8d93cdf6f1ed34a218523f7
jdaufin <jdaufin@student.42.fr>
2020-10-23 15:34:02 +0200

    Remove mapping of IPv4 to IPv6 addresses

--- srcs/resolve_address.c
+++ srcs/resolve_address.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <jdaufin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 10:24:03 by jdaufin           #+#    #+#             */
-/*   Updated: 2020/10/23 12:29:57 by jdaufin          ###   ########lyon.fr   */
+/*   Updated: 2020/10/23 15:31:34 by jdaufin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void			set_hints(t_addrinfo *hints)
 {
 	hints->ai_family = g_options.ai_family;
 	hints->ai_socktype = SOCK_RAW;
-	hints->ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;
+	hints->ai_flags = AI_ADDRCONFIG;
 	hints->ai_protocol = IPPROTO_TCP;
 	hints->ai_canonname = NULL;
 	hints->ai_addr = NULL;
