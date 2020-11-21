75a9ae4ea61fb1d17f11cbbefdd0969052b4d8a7
jdaufin <jdaufin@student.42.fr>
2020-11-21 01:18:29 +0100

    Fixing dynarray_add reallocation statements

--- srcs/dynarray_add.c
+++ srcs/dynarray_add.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <jdaufin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 11:22:37 by jdaufin           #+#    #+#             */
-/*   Updated: 2020/11/19 15:43:31 by jdaufin          ###   ########lyon.fr   */
+/*   Updated: 2020/11/21 01:17:42 by jdaufin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ static _Bool	extend_array(t_dynarray *parray)
 {
 	size_t	new_size;
 
-	new_size = parray->capacity + dynarray_getbuflen();
-	if (!ft_realloc((void **)&(parray->array), parray->capacity, new_size))
+	new_size = (parray->capacity + dynarray_getbuflen());
+	if (!ft_realloc((void **)&parray->array, \
+		parray->capacity * sizeof(double), new_size * sizeof(double)))
 		return (0);
 	parray->capacity = new_size;
 	return (1);
@@ -32,7 +33,7 @@ _Bool			dynarray_add(t_dynarray *parray, double val)
 	if ((parray->last_val_index + 1) >= parray->capacity)
 		if (!extend_array(parray))
 			return (0);
-	val_index = (parray->last_val_index)++;
+	val_index = ++(parray->last_val_index);
 	parray->array[val_index] = val;
 	return (1);
 }
