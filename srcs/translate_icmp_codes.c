0f99c7878489cf0ace7409823d8dcee3ba9390d2
jdaufin <jdaufin@student.42.fr>
2021-04-29 21:29:49 +0200

    ICMP error messages fitting with real ping

--- srcs/translate_icmp_codes.c
+++ srcs/translate_icmp_codes.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <jdaufin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 20:40:38 by jdaufin           #+#    #+#             */
-/*   Updated: 2021/04/29 20:55:42 by jdaufin          ###   ########lyon.fr   */
+/*   Updated: 2021/04/29 21:28:43 by jdaufin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	translate_icmp_type(char *dst, uint8_t type)
 {
 	if (type == ICMP_DEST_UNREACH)
-		ft_strcpy(dst, "Destination Unreachable");
+		ft_strcpy(dst, "Destination Host Unreachable");
 	else if (type == ICMP_TIME_EXCEEDED)
-		ft_strcpy(dst, "Time-To-Live Exceeded");
+		ft_strcpy(dst, "Time to live exceeded");
 	else if (type == ICMP_PARAMETERPROB)
 		ft_strcpy(dst, "Parameter problem");
 	else
