4f622a05411597503be9270a5d62c0bf8d9df0c3
jdaufin <jdaufin@student.42.fr>
2021-04-29 21:05:02 +0200

    Review file formatting

--- srcs/wait_cooldown.c
+++ srcs/wait_cooldown.c
@@ -6,21 +6,21 @@
 /*   By: jdaufin <jdaufin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 16:46:45 by jdaufin           #+#    #+#             */
-/*   Updated: 2021/04/29 17:00:35 by jdaufin          ###   ########lyon.fr   */
+/*   Updated: 2021/04/29 21:00:47 by jdaufin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-void            wait_cooldown(void)
+void	wait_cooldown(void)
 {
-    t_timeval	cursor;
-    time_t      end;
-    
-    gettimeofday(&cursor, NULL);
-    end = cursor.tv_sec + 1;
-    while (cursor.tv_sec < end)
-    {
-        gettimeofday(&cursor, NULL);
-    }
+	t_timeval	cursor;
+	time_t		end;
+
+	gettimeofday(&cursor, NULL);
+	end = cursor.tv_sec + 1;
+	while (cursor.tv_sec < end)
+	{
+		gettimeofday(&cursor, NULL);
+	}
 }
